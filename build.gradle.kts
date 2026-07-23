a6f0ed88a14bcc0f7f4ceefc1eb6193e941adb64
Philipp Gatzka <phil.gatzka@gmail.com>
2026-07-23 20:09:59 +0200

    #22 add publish step

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,16 @@ allprojects {
 
 }
 
-tasks.register<Exec>("installGitHooks") {
-    description = "Install git hooks"
-    commandLine = listOf("git", "config", "core.hooksPath", ".githooks")
+tasks{
+    register<Exec>("installGitHooks") {
+        description = "Install git hooks"
+        commandLine = listOf("git", "config", "core.hooksPath", ".githooks")
+    }
+    register("printVersion") {
+        group = "help"
+        description = "Prints the project version to quiet"
+        doLast {
+            logger.quiet(version.toString())
+        }
+    }
 }
