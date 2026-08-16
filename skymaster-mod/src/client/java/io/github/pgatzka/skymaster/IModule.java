845e51adc4af91febc6f88502907fe049e57b92d
Philipp Gatzka <phil.gatzka@gmail.com>
2026-08-16 12:23:55 +0200

    #67 remove everything

--- skymaster-mod/src/client/java/io/github/pgatzka/skymaster/IModule.java
+++
@@ -1,8 +0,0 @@
-package io.github.pgatzka.skymaster;
-
-public interface IModule {
-
-    default void onInitializeClient() {
-        // empty by default
-    }
-}
--- skymaster-server/src/main/java/io/github/pgatzka/skymaster/rest/exception/VersionMismatchException.java
+++
@@ -1,8 +0,0 @@
-package io.github.pgatzka.skymaster.rest.exception;
-
-public class VersionMismatchException extends RuntimeException {
-
-    public VersionMismatchException(String modVersion, String serverVersion) {
-        super("required version: " + serverVersion + ", actual version: " + modVersion);
-    }
-}
